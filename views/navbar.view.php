e46656e6af90f73ca5beacac5adfa53b75218ec1
kingofdevs <40970351+kingofdevs@users.noreply.github.com>
2020-04-10 11:04:44 +0800

    add logo

--- views/navbar.view.php
+++ views/navbar.view.php
@@ -40,27 +40,27 @@ $editHome = get_all_edit_home($connect);
             <div class="col-md-7">
                 <div id="colorlib-logo">
                     <a href="<?php echo SITE_URL; ?>/controller/index.php">
-                        <img src='<?php echo SITE_URL.'/images/'.$editHome[0]['logo'] ?>' title="MyPunter" class="logo-punter nopadding img-responsive" /> </a>
+                        <img src='<?php echo SITE_URL.'/images/logo.png' ?>' title="MyPunter" class="logo-punter nopadding img-responsive" /> </a>
                 </div>
             </div>
             <div class="col-md-3"></div>
             <div class="col-md-2 pt-17 responsive-center">
                 <span class="text-white font-weight font-size-13 ">Follow Us on</span>
                 <br />
-                <a href="https://www.facebook.com/mypunter/" title="MyPunter Facebook" target="_blank">
+                <a href="https://www.facebook.com/wwsfit/" title="MyPunter Facebook" target="_blank">
                     <span class="fa fa-facebook"></span>
                 </a>
-                <a href="https://twitter.com/mypunter" title="MyPunter Instagram" target="_blank">
+                <a href="https://twitter.com/wwsfit" title="MyPunter Instagram" target="_blank">
                     <span class="fa fa-twitter"></span>
                 </a>
-                <a href="https://instagram.com/my_punter/" title="MyPunter Instagram" target="_blank">
+                <a href="https://instagram.com/wwsfit/" title="MyPunter Instagram" target="_blank">
                     <span class="fa fa-instagram"></span>
                 </a>
                 <div class="mt-1"></div>
                 <span class="text-white ">Contact Us</span>
                 <br />
                 <div class="mt-007-"></div>
-                <a class="text-white underline font-size-13" href="mailto:support@mypunter.com?subject=Thanks&body=Best Regards">support@mypunter.com</a>
+                <a class="text-white underline font-size-13" href="mailto:support@wwsfit.com?subject=Thanks&body=Best Regards">support@wwsfit.com</a>
                 <br />
             </div>
         </div>
